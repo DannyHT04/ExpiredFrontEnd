81a425be71ccd0285ebfbca930cf51cb10bea64d
DannyHT04 <dannyhat1@gmail.com>
2022-04-08 15:49:22 -0700

    Co-authored-by: Jcontreras3 <Jcontreras3@users.noreply.github.com> Co-authored-by: Trent Barboza <trentbarboza@users.noreply.github.com>

---
+++ Screens/CreateAccountScreen.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { FC } from "react";
+
+const CreateAccountScreen: FC = () => {
+  return (
+  <View>
+  <Text>Create Account</Text>
+  </View>)
+};
+
+export default CreateAccountScreen;
---
+++ Screens/ProfileScreen.tsx
@@ -0,0 +1,10 @@
+import {Text} from 'react-native';
+import { FC } from 'react';
+const ProfileScreen: FC = () => {
+    return(
+        <Text>Create Account</Text>
+    );
+
+}
+
+export default ProfileScreen;
