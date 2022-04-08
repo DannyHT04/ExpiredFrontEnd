3a35a631bb8c1820059ad608d11c0cdecd7ee013
DannyHT04 <dannyhat1@gmail.com>
2022-04-08 15:52:03 -0700

    test

---
+++ Screens/ProfileScreen.tsx
@@ -0,0 +1,10 @@
+import {Text} from 'react-native';
+import { FC } from 'react';
+const ProfileScreen: FC = () => {
+    return(
+        <Text>Create Account test</Text>
+    );
+
+}
+
+export default ProfileScreen;
