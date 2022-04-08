290094bf827cf666c9b6c1625c6154e8c8627bf9
DannyHT04 <dannyhat1@gmail.com>
2022-04-08 15:53:06 -0700

    merge test

--- Screens/ProfileScreen.tsx
+++ Screens/ProfileScreen.tsx
@@ -2,11 +2,7 @@ import {Text} from 'react-native';
 import { FC } from 'react';
 const ProfileScreen: FC = () => {
     return(
-<<<<<<< HEAD
         <Text>Create Account test</Text>
-=======
-        <Text>Create Account</Text>
->>>>>>> origin/Danny
     );
 
 }
