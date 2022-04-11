513bf7cdd07fe0486936bf2241f7623f3bb436ca
Jovann Contreras <jcontreras@codestack.co>
2022-04-11 15:59:37 -0700

    Made most of layout for login

--- Screens/LoginScreen.tsx
+++ Screens/LoginScreen.tsx
@@ -1,31 +1,48 @@
 import { View, Text, StyleSheet } from "react-native";
 import { FC, useState } from "react";
 import { TextInput, Button } from "react-native-paper";
+import {
+  RobotoSlab_100Thin,
+  RobotoSlab_200ExtraLight,
+  RobotoSlab_300Light,
+  RobotoSlab_400Regular,
+  RobotoSlab_500Medium,
+  RobotoSlab_600SemiBold,
+  RobotoSlab_700Bold,
+  RobotoSlab_800ExtraBold,
+  RobotoSlab_900Black,
+} from "@expo-google-fonts/roboto-slab";
+
 const LoginScreen: FC = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   return (
-    <View style={styles.Bg1}>
-      <View style={[styles.Center, styles.BoxBg]}>
-        <Text style={styles.Fontsize}>Login</Text>
-      </View>
-      <View style={[styles.Mt3, styles.BoxBg]}>
-        <View>
+    <View style={styles.Center}>
+      <View style={[styles.BoxBg]}>
+        <Text style={[styles.Fontsize]}>Login</Text>
+
+        <View style={[styles.Mt2]}>
           <TextInput
+            style={styles.Mt1}
+            autoComplete="off"
             label="Username"
             value={username}
             onChangeText={setUsername}
           />
           <TextInput
+            autoComplete="off"
+            style={[styles.Mt1]}
             label="Password"
             value={password}
             onChangeText={setPassword}
           />
         </View>
-      </View>
-      <View style={styles.Mt1}>
-        <Button mode="contained">Log In</Button>
-        <Button>Create Account?</Button>
+        <View style={styles.Mt2}>
+          <Button color="#505050" mode="contained">
+            Log In
+          </Button>
+          <Button color="#405CBB">Create Account?</Button>
+        </View>
       </View>
     </View>
   );
@@ -33,26 +50,32 @@ const LoginScreen: FC = () => {
 const styles = StyleSheet.create({
   Center: {
     alignItems: "center",
+    backgroundColor: "#4B4B4B",
+    justifyContent: "center",
+    flex: 1,
   },
   Fontsize: {
     fontSize: 30,
     marginTop: 60,
-  },
-  Bg1: {
-    backgroundColor: "#4B4B4B",
-    flex: 2,
+    fontFamily: "RobotoSlab_400Regular",
   },
   Mt1: {
     marginTop: 30,
+    width: 300,
   },
   Mt2: {
-    marginTop: 30,
-  },
-  Mt3: {
     marginTop: 40,
   },
   BoxBg: {
     backgroundColor: "#9A9B9A",
+    flex: 1,
+    width: 350,
+    alignItems: "center",
+    marginTop: 40,
+    marginBottom: 40,
+  },
+  c1: {
+    color: "grey",
   },
 });
 
