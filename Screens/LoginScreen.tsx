e7ff5d83f3a79e26bbcf40c3cbecf5dcfbd34f5b
Jovann Contreras <jcontreras@codestack.co>
2022-04-11 14:49:13 -0700

    added input fields

--- Screens/LoginScreen.tsx
+++ Screens/LoginScreen.tsx
@@ -1,43 +1,75 @@
-import { View, Text, StyleSheet, TextInput } from "react-native";
-import { FC } from "react";
-
+import { View, Text, StyleSheet } from "react-native";
+import { FC, useState } from "react";
+import { TextInput, Button } from "react-native-paper";
 const LoginScreen: FC = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <View style={styles.Bg1}>
-      <View style={[styles.Padding, styles.BoxBg]}>
+      <View style={[styles.Center, styles.BoxBg]}>
         <Text style={styles.Fontsize}>Login</Text>
-        <TextInput style={styles.InputTxt}/>
       </View>
-          
+      <View style={[styles.Mt3, styles.BoxBg]}>
+        <View>
+          <TextInput
+            label="Username"
+            value={username}
+            onChangeText={setUsername}
+          />
+          <TextInput
+            label="Password"
+            value={password}
+            onChangeText={setPassword}
+          />
+        </View>
+      </View>
+      <View style={styles.Mt1}>
+        <Button mode="contained">Log In</Button>
+        <Button>Create Account?</Button>
+      </View>
     </View>
   );
 };
 const styles = StyleSheet.create({
-  Padding: {
-    
-    alignItems: "center"
+  Center: {
+    alignItems: "center",
   },
-  Fontsize:{
-      fontSize:30,
-      paddingTop: 30,
+  Fontsize: {
+    fontSize: 30,
+    marginTop: 60,
   },
-  Bg1:{
-      backgroundColor:'#4B4B4B',
-      flex:2
+  Bg1: {
+    backgroundColor: "#4B4B4B",
+    flex: 2,
   },
-  BoxBg:{
-      backgroundColor:'#9A9B9A',
-      width:360,
-      height: 580,
-      marginLeft:25,
-      marginTop:60
+  Mt1: {
+    marginTop: 30,
   },
-  InputTxt:{
-     borderBottomWidth:1,
-     padding:20
-  }
-
-
+  Mt2: {
+    marginTop: 30,
+  },
+  Mt3: {
+    marginTop: 40,
+  },
+  BoxBg: {
+    backgroundColor: "#9A9B9A",
+  },
 });
+
+//   BoxBg: {
+//     backgroundColor: "#9A9B9A",
+
+//     marginLeft: 25,
+//     marginTop: 60,
+//   },
+//   InputTxt: {
+//     height: 40,
+//     width:300,
+//     marginTop: 40,
+//     borderWidth: 1,
+//     backgroundColor:"white",
+//     borderColor:"#707070",
+//     padding: 10,
+//   },
 
 export default LoginScreen;
