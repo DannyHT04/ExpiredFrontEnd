d9fb201bd35a51991988c52de0d65119edf906cb
trentbarboza <tbarboza@codestack.co>
2022-04-13 11:03:23 -0700

    Added Logo to Profile

--- Screens/ProfileScreen.tsx
+++ Screens/ProfileScreen.tsx
@@ -1,10 +1,24 @@
-import {Text} from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
 import { FC } from 'react';
-const ProfileScreen: FC = () => {
-    return(
-        <Text>Create Account test</Text>
-    );
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Logo from '../assets/Logo.png';
 
+const ProfileScreen: FC = () => {
+    return (
+        <SafeAreaView style={styles.Bg}>
+            <Text>Create Account test</Text>
+            <Image source={Logo} style={{width: 104, height: 100}}/>
+        </SafeAreaView>
+    );
 }
+
+const styles = StyleSheet.create({
+    Bg: {
+        backgroundColor: "#4B4B4B",
+        flex: 1,
+        alignItems: "center"
+    }
+
+})
 
 export default ProfileScreen;
