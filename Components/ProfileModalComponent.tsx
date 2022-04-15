61367fa6c69f5c755d8b82611a7be18955ee6ab2
trentbarboza <tbarboza@codestack.co>
2022-04-15 14:13:09 -0700

    profile modals made

--- Components/ProfileModalComponent.tsx
+++ Components/ProfileModalComponent.tsx
@@ -1,25 +1,25 @@
-import * as React from 'react';
-import { Modal, Portal, Text, Button, Provider } from 'react-native-paper';
+// import * as React from 'react';
+// import { Modal, Portal, Text, Button, Provider } from 'react-native-paper';
 
-const ProfileModalComponent = () => {
-  const [visible, setVisible] = React.useState(false);
+// const ProfileModalComponent = () => {
+//   const [visible, setVisible] = React.useState(false);
 
-  const showModal = () => setVisible(true);
-  const hideModal = () => setVisible(false);
-  const containerStyle = {backgroundColor: 'white', padding: 20};
+//   const showModal = () => setVisible(true);
+//   const hideModal = () => setVisible(false);
+//   const containerStyle = {backgroundColor: '#303030', padding: 20};
 
-  return (
-    <Provider>
-      <Portal>
-        <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
-          <Text>Example Modal.  Click outside this area to dismiss.</Text>
-        </Modal>
-      </Portal>
-      <Button style={{marginTop: 30}} onPress={showModal}>
-        Show
-      </Button>
-    </Provider>
-  );
-};
+//   return (
+//     <Provider>
+//       <Portal>
+//         <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
+//           <Text style={{color: "white"}}>Edit Name:</Text>
+//         </Modal>
+//       </Portal>
+//       <Button style={{marginTop: 30}} onPress={showModal}>
+//         Show
+//       </Button>
+//     </Provider>
+//   );
+// };
 
-export default ProfileModalComponent;
+// export default ProfileModalComponent;
